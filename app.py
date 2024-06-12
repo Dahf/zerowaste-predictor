52658dd768cfabbbadf0694a47661f6e8672519f
Dahf <flacezockt@gmail.com>
2024-06-13 01:05:52 +0200

    ad

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ from PIL import Image
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 import torch
 from io import BytesIO
+import os
 
 app = Flask(__name__)
 
 # Laden Sie das Modell
 model = VisionEncoderDecoderModel.from_pretrained('sk_invoice_receipts')
-feature_extractor = ViTImageProcessor.from_pretrained(
-    'sk_invoice_receipts')
+feature_extractor = ViTImageProcessor.from_pretrained('sk_invoice_receipts')
 tokenizer = AutoTokenizer.from_pretrained('sk_invoice_receipts')
 
 device = torch.device('cpu')
 model.to(device)
+
+# Ordner zum Speichern der Bilder
+IMAGE_SAVE_PATH = "saved_images"
+os.makedirs(IMAGE_SAVE_PATH, exist_ok=True)
 
 @app.route('/')
 def home():
@@ -24,6 +28,10 @@ def predict():
     # Get image buffer from request
     image_data = request.data
     image = Image.open(BytesIO(image_data))
+
+    # Bild auf dem Server speichern
+    image_filename = os.path.join(IMAGE_SAVE_PATH, 'uploaded_image.png')
+    image.save(image_filename)
 
     # Preprocess the image
     pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values.to(device)
