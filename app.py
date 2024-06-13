646de78af255bea57678a2d55d566c766b25637f
Dahf <flacezockt@gmail.com>
2024-06-13 10:15:16 +0200

    asd

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
 from PIL import Image
-from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+from transformers import VisionEncoderDecoderModel, DonutProcessor
 import torch
 from io import BytesIO
 import os
+import re
 
 app = Flask(__name__)
 
 # Laden Sie das Modell
 model = VisionEncoderDecoderModel.from_pretrained('sk_invoice_receipts')
-feature_extractor = ViTImageProcessor.from_pretrained('sk_invoice_receipts')
-tokenizer = AutoTokenizer.from_pretrained('sk_invoice_receipts')
+processor = DonutProcessor.from_pretrained('sk_invoice_receipts')
 
 device = torch.device('cpu')
 model.to(device)
@@ -34,14 +34,28 @@ def predict():
     image.save(image_filename)
 
     # Preprocess the image
-    pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values.to(device)
-    
-    # Generate predictions
-    output_ids = model.generate(pixel_values)
-    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    preds = [pred.strip() for pred in preds]
-    
-    return jsonify({'prediction': preds[0]})
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values.to(device)
+    model.eval()
+    with torch.no_grad():
+
+        decoder_input_ids = processor.tokenizer(add_special_tokens=False, return_tensors="pt").input_ids
+        decoder_input_ids = decoder_input_ids.to(device)
+        generated_outputs = model.generate(
+            pixel_values,
+            decoder_input_ids=decoder_input_ids,
+            max_length=model.decoder.config.max_position_embeddings, 
+            pad_token_id=processor.tokenizer.pad_token_id,
+            eos_token_id=processor.tokenizer.eos_token_id,
+            early_stopping=True,
+            bad_words_ids=[[processor.tokenizer.unk_token_id]],
+            return_dict_in_generate=True
+        )
+
+        decoded_text = processor.batch_decode(generated_outputs.sequences)[0]
+        decoded_text = decoded_text.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+        decoded_text = re.sub(r"<.*?>", "", decoded_text, count=1).strip()  # remove first task start token
+        decoded_text = processor.token2json(decoded_text)
+        return decoded_text
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
